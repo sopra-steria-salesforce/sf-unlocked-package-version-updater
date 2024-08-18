c02b74d9ad11ad40074ee32e568c6216075e076a
John Føreland <john.foreland@me.com>
2024-08-18 18:37:59 +0200

    ignore when a package does not contain a version number

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ const sfdxJson = require(path + '/sfdx-project.json');
 
 async function run() {
     try {
-
         // We need all package directories
         const packageDirectories = sfdxJson.packageDirectories;
 
         packageDirectories.forEach(dir => {
-
             const versionNumber = dir.versionNumber;
-            const versionNumberSplit = versionNumber.split(".");
+            if (!versionNumber) return;
+
+            const versionNumberSplit = versionNumber.split('.');
             const minorVersion = versionNumberSplit[1];
             const newMinorVersionNumber = parseInt(minorVersion) + 1;
-            const newVersionNumber = versionNumberSplit[0] + "." + newMinorVersionNumber.toString() + ".0.NEXT"
+            const newVersionNumber =
+                versionNumberSplit[0] +
+                '.' +
+                newMinorVersionNumber.toString() +
+                '.0.NEXT';
             dir.versionNumber = newVersionNumber;
         });
 
@@ -25,8 +29,6 @@ async function run() {
             JSON.stringify(sfdxJson, null, 4).concat('\n'),
             'utf8'
         );
-
-        core.setOutput('isSuccess', true);
     } catch (error) {
         core.setFailed(error.message);
     }
