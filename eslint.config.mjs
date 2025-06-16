88792f68d0009bb40338bde28339fb4fdd3a968a
John Arne Føreland <john.foreland@me.com>
2025-06-16 13:16:31 +0200

    ignore release.config.mjs when linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,7 +21,7 @@ const compat = new FlatCompat({
 
 export default [
   {
-    ignores: ['**/coverage', '**/dist', '**/linter', '**/node_modules']
+    ignores: ['**/coverage', '**/dist', '**/linter', '**/node_modules', 'release.config.mjs']
   },
   ...compat.extends(
     'eslint:recommended',
